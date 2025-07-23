a2afd7b6969fef7abfb037680c7b4a68753904a2
David LaMartina <dlamart@amazon.com>
2025-07-23 14:08:26 -0500

    Cleared out day1

--- java-kata/src/main/java/com/kata/day1/BubbleSort.java
+++
@@ -1,9 +0,0 @@
-package com.kata.day1;
-
-import com.kata.types.*;
-import java.util.*;
-
-public class BubbleSort {
-    public static void bubbleSort(int[] arr) {
-    }
-}
--- java-kata/src/main/java/com/kata/day1/QuickSort.java
+++
@@ -1,9 +0,0 @@
-package com.kata.day1;
-
-import com.kata.types.*;
-import java.util.*;
-
-public class QuickSort {
-    public static void quickSort(int[] arr) {
-    }
-}
